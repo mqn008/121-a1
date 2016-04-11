922293423a2b35599c7c8f2a306b4b2c6fdd2d6e
Michelle Nguyen <mqn008@gmail.com>
2016-04-10 17:28:18 -0700

    delete, posting, sidebar changes

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ $(document).ready(function(){
 	var toggle = false;
 	$('.txtthere').hide();
 
+	$('.deletepost').click(function(){
+		$(this).parent().hide(200);
+	});
+
 	$('.toggle').click(function() {
 		if ( !toggle ) {
 			$(this).addClass('rotate');
-			$('.txtthere').show(1000);
+			$('.txtthere').show(500);
+			$('.toggletxt').hide(100);
 			toggle = true;
 		}
 		else {
 			$(this).removeClass('rotate');
-			$('.txtthere').hide(1000);
+			$('.txtthere').hide(100);			
+			$('.toggletxt').show(500);
+			$('.txtthere').val('');
 			toggle = false;
 		}
 	});
