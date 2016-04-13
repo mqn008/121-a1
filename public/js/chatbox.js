3a50b9a4a4e521b0f44364c81d984f1f03564454
Michelle Nguyen <mqn008@gmail.com>
2016-04-13 02:18:03 -0700

    working on sockets

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -1,89 +1,65 @@
 (function($) {
  
     "use strict";
+    var parsedData;
     /* TODO: Start your Javascript code here */
-    var socket =io();
-    socket.on('sidebar', function(profile){
-        var parsedData =  profile;
-        //$('#sidebar').append($('<li>').html(messageTemplate(parsedData)));
-        //function messageTemplate(template) {
-        //var result = '<div class="sidebar">' +
-            //'<img src="' + template.photos[0].value + '" alt="">' +
-           // '</div>';
-        //return result;
-       // }
+    var socket = io();
+    socket.on('sessions', function(profile){
+        parsedData = profile;
+        console.log("herlp");
+        console.log(parsedData);
     });
-        
-        /*$('#sidebar').append($('<li>').html(messageTemplate(parsedData)));
-=======
- 
-        $('#sidebar').append($('<li>').html(messageTemplate(parsedData)));
->>>>>>> f099c7c08b1f4b82027ac512603aa6366b71d420
-        function messageTemplate(template) {
-        var result = '<div class="sidebar">' +
-        '<img src="' + template.photos[0].value + '" alt="">' +
-         '</div>';
-        return result;
-<<<<<<< HEAD
-        }
-    });*/
 
-  /*  $('#post').submit(function(){
-=======
-       }
-    });
- 
-    $('#send_message').submit(function(){
-   
->>>>>>> f099c7c08b1f4b82027ac512603aa6366b71d420
-        // socket.emit sends out chat message events with attached data. In this case the submitted form data from #user.input
-        socket.emit('chat message', $('#user_input').val());
-        
-        $('#messages').append($('<li>').text($('#user_input').val()));
+    $('#post').submit(function(e) {
+        e.preventDefault();
+        console.log('DEEEEERP');
+        if( $('#user_input').val() == "") return false;
+
+        socket.emit('chat message', {"msg": $('#user_input').val(), "url": $('#url').val(), "titles": $('#title').val()});
         $('#user_input').val('');
-     
-         $('#messages').append($('<li>').html(messageTemplate(parsedData)));
-    return false;
-         // grab and parse data and assign it to the parsedData variable.
-        //updating new message
+        $('#url').val('');
+        $('#title').val('');
+
+        if( $('#url').val() == '' || $('#title').val() == '' ) {
+            $('#tweets').prepend(messageTemplate2(parsedData));
+        }
+
+        else $('#tweets').prepend(messageTemplate(parsedData));
+        
+        console.log("HELP");
+        return false;
+
         function messageTemplate(template) {
-        var result = '<div class="user">' +
-            '<div class="user-image">' +
-            '<img src="' + template.photo + '" alt="">' +
-            '</div>' +
-            '<div class="user-info">' +
-            '<span class="username">' + template.username + '</span><br/>' +
-            '<span class="posted">' + template.posted + '</span>' +
-            '</div>' +
-            '</div>' +
-            '<div class="message-content">' + $('#user_input').val() +
+            var result = '<div class="entry">'+
+            '<img src="' + template.photo + '" class="icon"/>' +
+            '<div class="text">' +
+                '<span class="name">' + template.username + '</span>' +
+                '<span class="at">@' + template.username + '</span>' +
+                '<div class="description">' + $('user_input').val() + '</div>' +
+                '<b>listen:</b> <a href="' + $('#url').val() + 
+                '" class="music">' + $('#title').val() +'</a>' +
+                '</div>' +
+                '<div class="interact">' +
+                    'LIKE DISLIKE COMMENT' +
+                '</div>' +
             '</div>';
 
             return result;
         }
-       
-    });
-    socket.on('new message', function(msg){
-        var parsedData =  msg;
-        $('#messages').append($('<li>').text(msg));
-        $('#messages').append($('<li>').html(messageTemplate(parsedData)));
-    
-        function messageTemplate(template) {
-        var result = '<div id="sidebar">' +
-            '<div class="myname"> ' +
-            '<img src="' + template.photo + '" alt="">' +
-            '</div>' +
-            '<div class="user-info">' +
-            '<span class="username">' + template.user + '</span><br/>' +
-            '<span class="posted">' + template.posted + '</span>' +
-            '</div>' +
-            '</div>' +
-            '<div class="message-content">' + template.message +
+
+        function messageTemplate2(template) {
+            var result = '<div class="entry">'+
+            '<img src="' + template.photo + '" class="icon"/>' +
+            '<div class="text">' +
+                '<span class="name">' + template.username + '</span>' +
+                '<span class="at">@' + template.username + '</span>' +
+                '<div class="description">' + $('user_input').val() + '</div>' +
+                '<div class="interact">' +
+                    'LIKE DISLIKE COMMENT' +
+                '</div>' +
             '</div>';
-        return result;
+
+            return result;
         }
-   
     });
-   
-   
 })($);
