58ecaa1dc87d32cc6b44e458c7944d3bbc5c2c92
Allan <companion.mail@gmail.com>
2016-04-13 12:47:45 -0700

    fixed sidebar profile display

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -23,7 +23,6 @@
         //console.log($('#user_input').val());
         // socket.emit sends out chat message events with attached data. In this case the submitted form data from #user.input
         socket.emit('post submit', $('#user_input').val());
-        $('#messages').append($('<li>').text($('#user_input').val()));
         $('#user_input').val('');
     return false;
     });
